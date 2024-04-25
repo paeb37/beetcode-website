a9bf6db775d8d17a8ffc2fc6c02fbdf827fc2a9e
paeb37 <btp2109@columbia.edu>
2024-04-25 16:13:19 -0400

    first push

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector(".navbar")
+
+/**
+window.onscroll = function() { // when the user scrolls far enough, we want the nav bar to be dark
+    var top = window.scrollY; // y pos of scroll/mouse
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+*/
